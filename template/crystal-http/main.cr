77c15d443594f633e1c658390f0bc7968b521bf6
Alexis Reigel <mail@koffeinfrei.org>
2019-10-14 22:30:32 +0200

    compiler isn't happy about the fuzzy has_key?
    we still get an error when using this with a NamedTuple that doesn't
    contain the key:
        Error: missing key 'status_code' for named tuple NamedTuple(body: String)

--- template/crystal-http/main.cr
+++ template/crystal-http/main.cr
@@ -13,17 +13,26 @@ server = HTTP::Server.new do |context|
   handler = Handler.new
   response_triple = handler.run(context.request)
 
-  if response_triple[:status_code]?
+  if response_triple.is_a?(NamedTuple(body: String, headers: HTTP::Headers, status_code: Int32) |
+                           NamedTuple(body: String, status_code: Int32) |
+                           NamedTuple(headers: HTTP::Headers, status_code: Int32) |
+                           NamedTuple(status_code: Int32))
     context.response.status_code = response_triple[:status_code]
   end
 
-  if response_triple.has_key?(:headers)
+  if response_triple.is_a?(NamedTuple(body: String, headers: HTTP::Headers, status_code: Int32) |
+                           NamedTuple(body: String, headers: HTTP::Headers) |
+                           NamedTuple(headers: HTTP::Headers, status_code: Int32) |
+                           NamedTuple(headers: HTTP::Headers))
     response_triple[:headers].each do |key, value|
       context.response.headers[key] = value
     end
   end
 
-  if response_triple.has_key?(:body)
+  if response_triple.is_a?(NamedTuple(body: String, headers: HTTP::Headers, status_code: Int32) |
+                           NamedTuple(body: String, headers: HTTP::Headers) |
+                           NamedTuple(body: String, status_code: Int32) |
+                           NamedTuple(body: String))
     context.response.print(response_triple[:body])
   end
 end
